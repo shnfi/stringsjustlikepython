695f370c2071d9e8560bd4ecd072741e757e3394
shnfi <mrplom7@gmail.com>
2025-03-19 04:23:08 +0330

    main library header/implementaion

---
+++ include/stringsjustlikepython.h
@@ -0,0 +1,14 @@
+/*
+ * MIT License
+ * Copyright (C) 2025 shnfi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy...
+ */
+
+#ifndef STRINGSJUSTLIKEPYTHON_H
+#define STRINGSJUSTLIKEPYTHON_H
+
+#include "capitalize.h"
+#include "casefold.h"
+
+#endif
