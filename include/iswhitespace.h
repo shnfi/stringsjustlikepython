564a1a5235ddca376a9ea210c96b2020869a6c4f
shnfi <mrplom7@gmail.com>
2025-03-23 18:26:28 +0330

    iswhitespace() method header/implementation

---
+++ include/iswhitespace.h
@@ -0,0 +1,6 @@
+#ifndef ISWHITESPACE_H
+#define ISWHITESPACE_H
+
+int iswhitespace(char *str);
+
+#endif
---
+++ src/iswhitespace.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../include/get_len_.h"
+#include "../include/iswhitespace.h"\
+
+int iswhitespace(char *str)
+{
+    for (int i = 0; i < get_len_(str); i++)
+        if (str[i] != ' ')
+            return 0;
+    
+    return 1;
+}
