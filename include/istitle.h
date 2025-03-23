1c4bd4f15add7cf4543ed1721859ece3787496e2
shnfi <mrplom7@gmail.com>
2025-03-23 18:25:52 +0330

    istitle() method header/implementation

---
+++ include/istitle.h
@@ -0,0 +1,8 @@
+#ifndef ISTITLE_H
+#define ISTITLE_H
+
+#define L_COUNT 26
+
+int istitle(char *str);
+
+#endif
---
+++ src/istitle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "../include/get_len_.h"
+#include "../include/istitle.h"
+
+extern const char SMALL_L[L_COUNT];
+extern const char CAPITAL_L[L_COUNT];
+
+int istitle(char *str)
+{
+    int qualify;
+
+    for (int i = 0; i < L_COUNT; i++)
+    {
+        if (str[0] == CAPITAL_L[i])
+        {
+            qualify = 1;
+            break;
+        }
+        else
+            qualify = 0;
+    }
+
+    if (!qualify)
+        return 1;
+
+    for (int i = 1; i < get_len_(str); i++)
+        for (int j = 0; j < L_COUNT; j++)
+            if (str[i] == CAPITAL_L[j])
+                return 0;
+
+    return qualify;
+}
