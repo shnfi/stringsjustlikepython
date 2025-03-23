51da787b58b8786d4c77f90a5f5ce384e3581872
shnfi <mrplom7@gmail.com>
2025-03-23 16:00:28 +0330

    islowercase() method header/implementation

---
+++ include/islowercase.h
@@ -0,0 +1,8 @@
+#ifndef ISLOWERCASE_H
+#define ISLOWERCASE_H
+
+#define L_COUNT 26
+
+int islowercase(char *str);
+
+#endif
---
+++ src/islowercase.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "../include/get_len_.h"
+#include "../include/islowercase.h"
+
+extern const char SMALL_L[L_COUNT];
+
+int islowercase(char *str)
+{
+    int qualify;
+
+    for (int i = 0; i < get_len_(str); i++)
+    {
+        for (int j = 0; j < L_COUNT; j++)
+        {
+            if (str[i] == SMALL_L[j])
+            {
+                qualify = 1;
+                break;
+            }
+            else
+                qualify = 0;
+        }
+
+        if (qualify == 0)
+            return qualify;
+    }
+    
+    return 1;
+}
