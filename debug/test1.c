18b3c87948b4200f21910d1fcde616c6e5c1f3c2
shnfi <mrplom7@gmail.com>
2025-03-23 15:07:33 +0330

    debug

--- debug/test1.c
+++ debug/test1.c
@@ -18,9 +18,19 @@ int main()
 
     printf("%d\n", find("hello, im shnfi!", 's'));
 
-    printf("%d\n", isallnum("78493"));
+    printf("%d\n", isallpha("aajfjkd0")); // expected output: 0
 
-    printf("%d\n", isallpha("aajfjkd0"));
+    printf("%d\n", isdecimal("43o78")); // expected output: 0
+
+    printf("%d\n", isidentifier("var")); // expected output: 1
+    printf("%d\n", isidentifier("_var")); // expected output: 1
+    printf("%d\n", isidentifier("v ar")); // expected output: 0
+    printf("%d\n", isidentifier(" var")); // expected output: 0
+
+    printf("%d\n", isallnum("abc")); // expected output: 1
+    printf("%d\n", isallnum("123")); // expected output: 1
+    printf("%d\n", isallnum("abc123")); // expected output: 1
+    printf("%d\n", isallnum("ab/c12")); // expected output: 0
     
     return 0;
 }
