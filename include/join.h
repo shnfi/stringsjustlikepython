2f61377ae6fd2973f6f0d33475ae9aa65b99c5a0
shnfi <mrplom7@gmail.com>
2025-03-24 17:46:19 +0330

    join() method header/implementation

---
+++ include/join.h
@@ -0,0 +1,6 @@
+#ifndef JOIN_H
+#define JOIN_H
+
+char *join(char *arr[], unsigned int len, char sep);
+
+#endif
---
+++ src/join.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/get_len_.h"
+#include "../include/join.h"
+
+char *join(char *arr[], unsigned int len, char sep)
+{
+    /*
+     * the buff_size variable is for getting the byte count that 
+     * we should allocate memory for the out variable.
+     * 
+     * why it's declared to "len - 1"?
+     * the len variable is the len of the whole aray, so our separators
+     * count will be len - 1.
+     */
+
+    int buff_size = len - 1;
+
+    for (int i = 0; i < len; i++)
+        for (int j = 0; j < get_len_(arr[i]); j++)
+            buff_size++;
+
+    char *out = malloc(buff_size);
+
+    /*
+     * for each string in the array, we append the letter to the 
+     * out variable and after appending a whole string, we will append
+     * the separator.
+     */
+
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < get_len_(arr[i]); j++)
+        {
+            out[get_len_(out)] = arr[i][j];
+        }
+
+        /*
+         * append separator if its not the last loop
+         */
+
+        if (i != len - 1)
+            out[get_len_(out)] = sep;
+    }
+
+    return out;
+}
