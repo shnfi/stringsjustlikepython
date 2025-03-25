ed99bc647b6bbcc3549d8bb067d193bbec08153e
shnfi <mrplom7@gmail.com>
2025-03-25 16:49:07 +0330

    fixed a bug related to the colors

--- log.sh
+++ log.sh
@@ -5,7 +5,7 @@ log_color=$(echo $2)
 log_num=$(cat CHANGELOG | wc -l)
 
 if [[ $log_color -eq "green" || $log_color -eq "red" ]]; then
-	echo "<p style="color:$log_color">[$log_num] - $new_log</p>" >> CHANGELOG
+	echo "<p style='color:$log_color'>[$log_num] - $new_log</p>" >> CHANGELOG
 else
 	echo "<p>[$log_num] - $new_log</p>" >> CHANGELOG
 fi
