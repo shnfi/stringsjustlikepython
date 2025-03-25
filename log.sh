01686159bca3a868a6afad28556b1b41ff398db1
shnfi <mrplom7@gmail.com>
2025-03-25 16:38:29 +0330

    script for adding log to CHANGELOG

---
+++ log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+new_log=$(echo $1)
+log_color=$(echo $2)
+log_num=$(cat CHANGELOG | wc -l)
+
+if [[ $log_color -eq "green" || $log_color -eq "red" ]]; then
+	echo "<p style="color:$log_color">[$log_num] - $new_log</p>" >> CHANGELOG
+else
+	echo "<p>[$log_num] - $new_log</p>" >> CHANGELOG
+fi
